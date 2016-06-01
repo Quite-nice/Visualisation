1df290e54d0bb1a8ecb4e36b319f5e8394c74a5c
Kymer <kymer.dev@icloud.com>
2016-06-01 17:18:36 +0200

    Add imports folder and entry points
    - following structure as defined here: http://guide.meteor.com/structure.html

---
+++ Web application/imports/startup/client/index.js
@@ -0,0 +1,5 @@
+/**
+ * Created by Kymer on 1/06/16.
+ */
+
+// import client startup through a single index entry point
---
+++ Web application/imports/startup/server/fixtures.js
@@ -0,0 +1,5 @@
+/**
+ * Created by Kymer on 1/06/16.
+ */
+
+// fill the DB with example data on startup
