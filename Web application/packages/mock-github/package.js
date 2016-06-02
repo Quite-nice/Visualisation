50180471dfc47ba4eb518a95f7fa3fa33c2ef0b7
Damiaan Dufaux <info@devian.tk>
2016-06-02 16:02:30 +0200

    add documentation

--- Web application/packages/mock-github/package.js
+++ Web application/packages/mock-github/package.js
@@ -2,7 +2,7 @@ Package.describe({
 	name: 'mock-github',
 	version: '0.0.1',
 	// Brief, one-line summary of the package.
-	summary: '',
+	summary: 'Adds mock data to the modules and events collections',
 	// URL to the Git repository containing the source code for this package.
 	git: '',
 	// By default, Meteor will default to using README.md for documentation.
