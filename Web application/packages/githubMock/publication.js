2a1e7b2e68b7aeb5ee5c8847597ad969a0245d58
Damiaan Dufaux <info@devian.tk>
2016-06-02 15:39:40 +0200

    couple events with modules

--- Web application/packages/githubMock/publication.js
+++ Web application/packages/githubMock/publication.js
@@ -19,16 +19,19 @@ const events = [
 		hash: 'ab65',
 		author: 'Damiaan',
 		message: 'init',
-		date: new Date()
+		date: new Date(),
+		senderId: branchesId
 	}, {
 		hash: 'ab69',
 		author: 'Jeroen',
 		message: 'add spul',
-		date: new Date()
+		date: new Date(),
+		senderId: branchesId
 	}, {
 		hash: 'ab35',
 		author: 'Kymer',
 		message: 'add ding',
-		date: new Date()
+		date: new Date(),
+		senderId: branchesId
 	}
 ];
