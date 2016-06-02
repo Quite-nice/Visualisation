1ebb22f39eb7b6eaa97e07c7f1d6f441ed4a4251
Jeroen <jeroen.berrevoets@gmail.com>
2016-06-02 14:34:06 +0200

    First Draft of Generic UI -> Testing required

--- Web application/imports/ui/components/module/module.js
+++ Web application/imports/ui/components/module/module.js
@@ -1,3 +1,19 @@
 /**
  * Created by Jeroen on 02/06/16.
  */
+import 'module.html'
+
+import {Events} from '/imports/api/events/events'
+import {Modules} from '/imports/api/modules/modules'
+
+Template.GenericModule.onCreated(function (){
+	Meteor.subscribe('eventsReceivedByModule', this._id)
+})
+
+Template.GenericModule.helpers({
+	receivedEvents(){
+		// best to filter on the sender, this way, shouts don't get excluded
+		Events.find({sender: {$ne: this._id}})
+	}
+})
+
