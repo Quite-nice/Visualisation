c636c6ec40611546d3a5e08e448000bfca9a318d
Damiaan Dufaux <info@devian.tk>
2016-06-02 14:30:42 +0200

    use single quotes

--- Zyre mediator/Mongo mediator.py
+++ Zyre mediator/Mongo mediator.py
@@ -38,17 +38,18 @@ def rethinkdb_writer(ctx, pipe):
     meteor['modules'].insert([{
         "_id": str(n.uuid()),
         "name": n.name(),
-        "moduleType": configuration['zyreMediator']['type']
+        "type": configuration['zyreMediator']['type'],
+
     }])
 
     ready_message = {
-        "type": "state",
-        "sender_UUID": str(n.uuid()),
-        "payload": 2
+        'type': 'state',
+        'sender_UUID': str(n.uuid()),
+        'payload': 2
     }
 
     def logMessage(message_to_log):
-        message_to_log["timestamp"] = datetime.datetime.utcnow()
+        message_to_log['timestamp'] = datetime.datetime.utcnow()
         meteor['events'].insert_one(message_to_log)
         del message_to_log['timestamp']
         del message_to_log['_id']
@@ -64,7 +65,7 @@ def rethinkdb_writer(ctx, pipe):
         if pipe in items and items[pipe] == zmq.POLLIN:
             message = pipe.recv()
             # message to quit
-            if message.decode('utf-8') == "$$STOP":
+            if message.decode('utf-8') == '$$STOP':
                 break
 
         if n.inbox in items and items[n.inbox] == zmq.POLLIN:
@@ -73,63 +74,63 @@ def rethinkdb_writer(ctx, pipe):
             msg_type = msg_frame.pop(0)
             peer_uid = uuid.UUID(bytes=msg_frame.pop(0))
             peer_name = msg_frame.pop(0)
-            # print("NODE_MSG TYPE: %s" % msg_type)
-            # print("NODE_MSG PEER: %s" % str(peer_uid))
-            # print("NODE_MSG NAME: %s" % peer_name)
+            # print('NODE_MSG TYPE: %s' % msg_type)
+            # print('NODE_MSG PEER: %s' % str(peer_uid))
+            # print('NODE_MSG NAME: %s' % peer_name)
 
-            if msg_type.decode('utf-8') == "ENTER":
+            if msg_type.decode('utf-8') == 'ENTER':
 
                 headers = json.loads(msg_frame.pop(0))
 
                 try:
-                    module_type = headers["type"]
+                    module_type = headers['type']
                 except KeyError:
                     print("Your header doesn't contain your type of robot")
-                    module_type = "unknown"
+                    module_type = 'unknown'
 
                 # creates an entry with all known information about the robot
                 # in the database if the robot is not in the database
-                meteor["modules"].insert_one({
-                    "_id": str(peer_uid),
-                    "name": peer_name,
-                    "moduleType": module_type
+                meteor['modules'].insert_one({
+                    '_id': str(peer_uid),
+                    'name': peer_name,
+                    'type': module_type
                 })
 
                 module_name_to_uid_map[peer_name] = str(peer_uid)
 
-            elif msg_type.decode('utf-8') == "EXIT":
-                meteor["modules"].remove({"uuid": str(peer_uid)})
+            elif msg_type.decode('utf-8') == 'EXIT':
+                meteor['modules'].remove({'uuid': str(peer_uid)})
 
-            elif msg_type.decode('utf-8') == "SHOUT":
+            elif msg_type.decode('utf-8') == 'SHOUT':
 
                 # write message to database
                 group = msg_frame.pop(0)
                 try:
                     data = json.loads(msg_frame[0])
                 except:
-                    print "Invalid JSON string"
+                    print 'Invalid JSON string'
 
                 # print data
 
-                data["sender_UUID"] = str(peer_uid)
+                data['sender_UUID'] = str(peer_uid)
                 logMessage(data)
 
-            elif msg_type.decode('utf-8') == "WHISPER":
+            elif msg_type.decode('utf-8') == 'WHISPER':
                 # write message to database
                 try:
                     data = json.loads(msg_frame[0])
                 except:
-                    print "Invalid JSON string"
+                    print 'Invalid JSON string'
 
                 logMessage(data)
 
-    meteor["modules"].remove({"_id": str(n.uuid())})
+    meteor['modules'].remove({'_id': str(n.uuid())})
     n.stop()
 
 
 if __name__ == '__main__':
     # Create a StreamHandler for debugging
-    logger = logging.getLogger("pyre")
+    logger = logging.getLogger('pyre')
     logger.setLevel(logging.INFO)
     logger.addHandler(logging.StreamHandler())
     logger.propagate = False
@@ -145,6 +146,6 @@ if __name__ == '__main__':
         except (KeyboardInterrupt, SystemExit):
             break
 
-    database_writer.send("$$STOP".encode('utf_8'))
+    database_writer.send('$$STOP'.encode('utf_8'))
 
-    print("FINISHED")
+    print('FINISHED')
