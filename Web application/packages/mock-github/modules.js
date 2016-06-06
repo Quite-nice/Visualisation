1df14db164295df3eb4e02a78be623136e41d97f
Kymer <kymer.dev@icloud.com>
2016-06-06 22:42:31 +0200

    Use typeId in type field of mock module

--- Web application/packages/mock-github/modules.js
+++ Web application/packages/mock-github/modules.js
@@ -1,20 +1,22 @@
+import {typeId1, type1, typeId2, type2, typeId3, type3} from './types'
+
 export const branchesId = new Mongo.ObjectID()
 export const branches = {
-	type: 'collection',
+	type: typeId1,
 	name: 'Branches',
 	parent: null
 };
 
 export const issuesId = new Mongo.ObjectID()
 export const issues = {
-	type: 'collection',
+	type: typeId2,
 	name: 'Issues',
 	parent: null
 };
 
 export const pullRequestsId = new Mongo.ObjectID()
 export const pullRequests = {
-	type: 'collection',
+	type: typeId3,
 	name: 'Pull requests',
 	parent: null
 };
