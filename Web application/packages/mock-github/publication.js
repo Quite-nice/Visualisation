759d8b5ae2c201a4a7196f950027998e74a51e43
Kymer <kymer.dev@icloud.com>
2016-06-06 22:42:58 +0200

    Explicitly import mock type related data

--- Web application/packages/mock-github/publication.js
+++ Web application/packages/mock-github/publication.js
@@ -3,12 +3,13 @@
 // Variables exported by this module can be imported by other packages and
 // applications. See tests.js for an example of importing.
 
-import {branches, branchesId, issuesId, issues} from './modules'
+import {branches, branchesId, issuesId, issues, pullRequests, pullRequestsId} from './modules'
 import {events} from './events'
 
 Meteor.publish('mockGithub', function() {
 	this.added('modules', branchesId, branches);
 	this.added('modules', issuesId, issues);
+	this.added('modules', pullRequestsId, pullRequests)
 
 	for (let event of events) {
 		this.added('events', new Mongo.ObjectID(), event);
