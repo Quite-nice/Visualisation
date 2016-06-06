8e70875c860be410a9d086fa5ed443675af5a5bb
Kymer <kymer.dev@icloud.com>
2016-06-06 23:11:52 +0200

    Add subModulesFromModule(id) (+type) publications

--- Web application/imports/api/modules/server/publications.js
+++ Web application/imports/api/modules/server/publications.js
@@ -25,6 +25,7 @@ Meteor.publishComposite('allModulesPlusType', {
 	}]
 })
 
+
 Meteor.publish('module', function(id) {
 	return Modules.find({_id: id})
 })
@@ -33,6 +34,26 @@ Meteor.publishComposite('modulePlusType', function(id) {
 	return {
 		find: function () {
 			return Modules.find({_id: id})
+		},
+		children : [{
+			find: function (module) {
+				// this needs to be a find() and not findOne() as required
+				// by the publish-composite package
+				return Types.find({_id: module.typeId})
+			}
+		}]
+	}
+})
+
+
+Meteor.publish('subModulesFromModule', function(id) {
+	return Modules.find({parentId: id})
+})
+
+Meteor.publishComposite('subModulesFromModulePlusType', function(id) {
+	return {
+		find: function () {
+			return Modules.find({parentId: id})
 		},
 		children : [{
 			find: function (module) {
