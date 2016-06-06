f9f6648a61271cc99ec7d088b922b71c74f25e15
Kymer <kymer.dev@icloud.com>
2016-06-07 00:23:52 +0200

    Add quick test to 'test' mocha set-up
    Note: should run app in test mode using this command: `meteor test --driver-package practicalmeteor:mocha --port 3010`

--- Web application/imports/api/events/server/publications.tests.js
+++ Web application/imports/api/events/server/publications.tests.js
@@ -1,5 +1,22 @@
 /**
  * Created by Kymer on 1/06/16.
  */
+import { Events } from '../events'
+import { assert } from 'meteor/practicalmeteor:chai'
+import { resetDatabase } from 'meteor/xolvio:cleaner'
 
 // tests for the events publications
+
+describe('Publications', function () {
+	beforeEach(function () {
+		resetDatabase()
+	});
+});
+
+describe('Events', () => {
+	describe('allEvents', () => {
+		it('should return all events', () => {
+			assert.equal(42, 42)
+		});
+	});
+});
