acfffc222d6a084920241a464a8440bb9e610ffd
Kymer <kymer.dev@icloud.com>
2016-06-06 22:52:36 +0200

    Remove limit in query
    (was for testing purposes)

--- Web application/imports/api/modules/server/publications.js
+++ Web application/imports/api/modules/server/publications.js
@@ -9,12 +9,12 @@ import { Types } from '/imports/api/types/types'
 // publications are available with and without type information
 // on the client you can selectively choose which publication is needed
 Meteor.publish('allModules', function() {
-	return Modules.find({}, {limit: 2})
+	return Modules.find()
 })
 
 Meteor.publishComposite('allModulesWithType', {
 	find: function () {
-		return Modules.find({}, {limit: 1})
+		return Modules.find()
 	},
 	children: [{
 		find: function(module) {
