128ffa563e90c0c9dfb1085b8d6f2015a06f2da9
Kymer <kymer.dev@icloud.com>
2016-06-06 22:41:53 +0200

    Add Types mock data

---
+++ Web application/packages/mock-github/types.js
@@ -0,0 +1,20 @@
+/**
+ * Created by Kymer on 6/06/16.
+ */
+export const typeId1 = new Mongo.ObjectID()
+export const type1 = {
+	name: 'Type one',
+	color: '#003761'
+};
+
+export const typeId2 = new Mongo.ObjectID()
+export const type2 = {
+	name: 'Type two',
+	color: '#F47923'
+};
+
+export const typeId3 = new Mongo.ObjectID()
+export const type3 = {
+	name: 'Type three',
+	color: '#65C178'
+};
