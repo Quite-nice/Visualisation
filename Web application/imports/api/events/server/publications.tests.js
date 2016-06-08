19f5a4d44acb73b093fff2a457267028a995b41c
Kymer <kymer.dev@icloud.com>
2016-06-09 00:50:43 +0200

    Test Events factory

--- Web application/imports/api/events/server/publications.tests.js
+++ Web application/imports/api/events/server/publications.tests.js
@@ -1,12 +1,38 @@
 /**
  * Created by Kymer on 1/06/16.
  */
+import {Meteor} from 'meteor/meteor'
 import {Events} from '/imports/api/events/events'
 import {assert} from 'meteor/practicalmeteor:chai'
 import {resetDatabase} from 'meteor/xolvio:cleaner'
 import faker from 'faker'
 
 // tests for the events publications
+
+describe('Events', function () {
+	beforeEach(function () {
+		resetDatabase()
+	});
+
+	describe('mutators', function () {
+		it('builds correctly from factory', function () {
+
+			// this should create 1 event in the Events collection
+			const event = Factory.create('event')
+			assert.equal(Events.find().count(), 1)
+
+			assert.typeOf(event, 'object')
+			assert.typeOf(event.senderId, 'string')
+			assert.typeOf(event.date, 'date')
+			assert.typeOf(event.type, 'string')
+
+			assert.typeOf(event.payload, 'object')
+			assert.typeOf(event.payload.hash, 'string')
+			assert.typeOf(event.payload.author, 'string')
+			assert.typeOf(event.payload.message, 'string')
+		})
+	})
+})
 
 describe('Events publications', function () {
 	beforeEach(function () {
@@ -15,7 +41,7 @@ describe('Events publications', function () {
 
 	describe('allEvents', () => {
 		it('should return all events', () => {
-			assert.equal(42, 42)
+			// todo: add package to test publications (e.g. johanbrook:publication-collector)
 		});
 	});
 });
