0d2fb98ea96646c20cf223a2ccc0721be0b7f208
Damiaan Dufaux <info@devian.tk>
2016-06-09 15:39:56 +0200

    Make core DB collections available for packages
    Problem: you cannot interact with the modules and event collections from inside a package.
    
    Solution: by moving them into a package 'database', package developers can depend on this to work with modules and events

--- Web application/imports/api/modules/modules.js
+++ Web application/imports/api/modules/modules.js
@@ -1,11 +1,6 @@
 /**
  * Created by Kymer on 1/06/16.
  */
-import {Meteor} from 'meteor/meteor'
+import {Meteor} from 'meteor/meteor';
 
-export const Modules = new Mongo.Collection('modules')
-
-// add Geospatial index so we can query it
-if(Meteor.isServer) {
-	Modules._ensureIndex({'loc.coordinates':'2dsphere'});
-}
+export {Modules} from 'meteor/database';
---
+++ Web application/packages/database/package.js
@@ -0,0 +1,24 @@
+Package.describe({
+  name: 'database',
+  version: '0.0.1',
+  // Brief, one-line summary of the package.
+  summary: '',
+  // URL to the Git repository containing the source code for this package.
+  git: '',
+  // By default, Meteor will default to using README.md for documentation.
+  // To avoid submitting documentation, set this field to null.
+  documentation: 'README.md'
+});
+
+Package.onUse(function(api) {
+  api.versionsFrom('1.3.2.4');
+  api.use(['ecmascript', 'mongo']);
+  api.mainModule('database.js');
+});
+
+Package.onTest(function(api) {
+  api.use('ecmascript');
+  api.use('tinytest');
+  api.use('database');
+  api.mainModule('database-tests.js');
+});
