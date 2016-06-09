4df7582efca416aeb40d9bea5ef7213593a3ebcd
Damiaan Dufaux <info@devian.tk>
2016-06-09 15:52:01 +0200

    create ios-mediator package

---
+++ Web application/packages/ios-mediator/package.js
@@ -0,0 +1,28 @@
+Package.describe({
+	name: 'ios-mediator',
+	version: '0.0.1',
+	// Brief, one-line summary of the package.
+	summary: 'Use meteor as a mediator to write messages and modules in the database',
+	// URL to the Git repository containing the source code for this package.
+	git: '',
+	// By default, Meteor will default to using README.md for documentation.
+	// To avoid submitting documentation, set this field to null.
+	documentation: 'README.md'
+});
+
+Package.onUse(function(api) {
+	api.versionsFrom('1.3.2.4');
+	api.use('ecmascript');
+	api.mainModule('ios-mediator.js');
+});
+
+Package.onTest(function(api) {
+	api.use('ecmascript');
+	api.use('tinytest');
+	api.use('ios-mediator');
+	api.mainModule('ios-mediator-tests.js');
+});
+
+Npm.depends({
+	bonjour: "^3.5.0"
+});
