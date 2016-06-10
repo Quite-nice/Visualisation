b01b78d33089f96e6a8e0e51a9a222f9f14c34d3
Damiaan Dufaux <info@devian.tk>
2016-06-10 16:33:25 +0200

    import publications

--- Web application/imports/startup/server/index.js
+++ Web application/imports/startup/server/index.js
@@ -4,5 +4,9 @@
 
 // import server startup through a single index entry point
 import '/imports/api/events/events.js'
+import '/imports/api/events/server/publications'
+
 import '/imports/api/modules/modules.js'
+import '/imports/api/modules/server/publications'
+
 import '/imports/api/types/types.js'
