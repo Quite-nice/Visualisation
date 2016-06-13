684d6fb74f8b447f019c8726a607b664a0b3e226
Damiaan Dufaux <info@devian.tk>
2016-06-13 12:16:30 +0200

    Revert "implemented a detail page"
    This reverts commit b86a3cf0ebde0cb32bbd2796dcddd12a07176273.

--- Web application/imports/ui/components/GenericModule/DetailView.js
+++ Web application/imports/ui/components/GenericModule/DetailView.js
@@ -3,19 +3,14 @@
  */
 
 import './DetailView.html'
-<<<<<<< Updated upstream
 import {Modules} from '/imports/api/modules/modules'
 import {Events} from '/imports/api/events/events'
 
 import '/imports/ui/components/JsonView/JsonView'
-=======
-import {findModuleDescriptorField, getDetailPageName} from '/imports/ui/extensions/registry'
->>>>>>> Stashed changes
 
 const template = Template.DetailView;
 
 template.helpers({
-<<<<<<< Updated upstream
     totalNumberOfModules(){
         return Modules.find().count()
     },
@@ -23,13 +18,3 @@ template.helpers({
         return Events.find().count()
     }
 });
-=======
-	pages() {
-		return findModuleDescriptorField(this.module, 'detailPageViews')
-	},
-	pageName(page) {
-		console.log(page)
-		return getDetailPageName(page)
-	}
-})
->>>>>>> Stashed changes
