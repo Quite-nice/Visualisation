de59a3831be4a35af16844cfe3724c94754bd43b
Damiaan Dufaux <info@devian.tk>
2016-06-15 00:22:21 +0200

    link packages with github

--- Web application/packages/iphonegyro/package.js
+++ Web application/packages/iphonegyro/package.js
@@ -4,10 +4,10 @@ Package.describe({
   // Brief, one-line summary of the package.
   summary: 'visualize gyro data coming from the iphone',
   // URL to the Git repository containing the source code for this package.
-  git: '',
+  git: 'https://github.com/Quite-nice/Visualisation/',
   // By default, Meteor will default to using README.md for documentation.
   // To avoid submitting documentation, set this field to null.
-  documentation: 'README.md'
+  documentation: null
 });
 
 Package.onUse(function(api) {
--- Web application/packages/websocket-mediator/package.js
+++ Web application/packages/websocket-mediator/package.js
@@ -4,10 +4,10 @@ Package.describe({
 	// Brief, one-line summary of the package.
 	summary: 'Use meteor as a mediator to write messages and modules in the database',
 	// URL to the Git repository containing the source code for this package.
-	git: '',
+	git: 'https://github.com/Quite-nice/Visualisation/',
 	// By default, Meteor will default to using README.md for documentation.
 	// To avoid submitting documentation, set this field to null.
-	documentation: 'README.md'
+	documentation: null
 });
 
 Package.onUse(function(api) {
