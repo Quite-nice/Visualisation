de8bd85e9cdb6f2eba9e36dafaa2c62230504141
Damiaan Dufaux <info@devian.tk>
2016-06-15 00:15:25 +0200

    add visualisation namespace

--- Web application/packages/iphonegyro/package.js
+++ Web application/packages/iphonegyro/package.js
@@ -21,6 +21,6 @@ Package.onUse(function(api) {
 Package.onTest(function(api) {
   api.use('ecmascript');
   api.use('tinytest');
-  api.use('iphonegyro');
+  api.use('visualisation:iphonegyro');
   api.mainModule('iphonegyro-tests.js');
 });
