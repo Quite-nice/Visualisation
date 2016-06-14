2029dcf2874e220d6cd5ecb50e62bdc98f2a47cd
Damiaan Dufaux <info@devian.tk>
2016-06-14 10:19:09 +0200

    create extension package

---
+++ Web application/packages/extension-system/extension-system-tests.js
@@ -0,0 +1,11 @@
+// Import Tinytest from the tinytest Meteor package.
+import { Tinytest } from "meteor/tinytest";
+
+// Import and rename a variable exported by extension-system.js.
+import { name as packageName } from "meteor/extension-system";
+
+// Write your tests here!
+// Here is an example.
+Tinytest.add('extension-system - example', function (test) {
+  test.equal(packageName, "extension-system");
+});
