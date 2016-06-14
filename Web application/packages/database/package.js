d0ca6b0fa89830afa8cabb170e8ff9db6ea4024a
Damiaan Dufaux <info@devian.tk>
2016-06-14 22:25:10 +0200

    update database test dependencies

--- Web application/packages/database/package.js
+++ Web application/packages/database/package.js
@@ -2,7 +2,7 @@ Package.describe({
   name: 'visualisation:database',
   version: '0.0.1',
   // Brief, one-line summary of the package.
-  summary: '',
+  summary: 'Modules and events collections for the visualisation platform',
   // URL to the Git repository containing the source code for this package.
   git: '',
   // By default, Meteor will default to using README.md for documentation.
@@ -21,6 +21,6 @@ Package.onUse(function(api) {
 Package.onTest(function(api) {
   api.use('ecmascript');
   api.use('tinytest');
-  api.use('database');
+  api.use('visualisation:database');
   api.mainModule('database-tests.js');
 });
