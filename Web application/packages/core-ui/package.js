a76dc038569e13eb700329d01046387d572f67e4
Damiaan Dufaux <info@devian.tk>
2016-06-23 12:23:10 +0200

    bump packages to 0.0.2

--- Web application/packages/core-ui/package.js
+++ Web application/packages/core-ui/package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'visualisation:core-ui',
-  version: '0.0.1',
+  version: '0.0.2',
   // Brief, one-line summary of the package.
   summary: 'Basic page layout with a menu bar',
   // URL to the Git repository containing the source code for this package.
