98bce7dfa5a77c130e4dccbebbbd8e19f3875ccf
Damiaan Dufaux <info@devian.tk>
2016-06-23 16:12:08 +0200

    add twitter bootstrap

--- Web application/packages/core-ui/package.js
+++ Web application/packages/core-ui/package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'visualisation:core-ui',
-  version: '0.0.2',
+  version: '0.0.3',
   // Brief, one-line summary of the package.
   summary: 'Basic page layout with a menu bar',
   // URL to the Git repository containing the source code for this package.
@@ -13,7 +13,14 @@ Package.describe({
 Package.onUse(function(api) {
   api.versionsFrom('1.3.3');
   api.use(['ecmascript', 'templating']);
-  api.use(['devian:navigation@0.0.3', 'kadira:blaze-layout@2.3.0', 'kadira:flow-router@2.12.1', 'visualisation:extension-system@0.0.1', 'visualisation:database@0.0.1']);
+  api.use([
+    'devian:navigation@0.0.3',
+    'kadira:blaze-layout@2.3.0',
+    'kadira:flow-router@2.12.1',
+    'visualisation:extension-system@0.0.1',
+    'visualisation:database@0.0.1',
+    'twbs:bootstrap@3.3.6'
+  ]);
   api.mainModule('core-ui.js', 'client');
 });
 
