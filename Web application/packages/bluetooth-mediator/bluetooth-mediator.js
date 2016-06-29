ca8b1eea7c91c585abd22332821cd7ef93b0ec29
Damiaan Dufaux <info@devian.tk>
2016-06-29 11:43:01 +0200

    add bluetooth mediator

---
+++ Web application/packages/bluetooth-mediator/bluetooth-mediator.js
@@ -0,0 +1,70 @@
+import {Modules, Events} from 'meteor/visualisation:database';
+
+const noble = require('noble');
+
+noble.on('stateChange', Meteor.bindEnvironment(function(state) {
+	if (state === 'poweredOn') {
+		noble.startScanning([], true);
+	} else {
+		noble.stopScanning();
+		removeAllModules()
+	}
+}));
+
+const deviceTimeouts = new Map();
+const timeout = 1000;
+
+removeAllModules();
+noble.on('discover', Meteor.bindEnvironment(function(peripheral) {
+	if (deviceTimeouts.has(peripheral.id)) {
+		clearTimeout(deviceTimeouts.get(peripheral.id));
+		deviceTimeouts.set(peripheral.id, Meteor.setTimeout(() => deleteTimeout(peripheral.id), timeout));
+		Modules.update(peripheral.id, {
+			$set: {name: peripheral.advertisement.localName}
+		})
+	} else {
+		deviceTimeouts.set(peripheral.id, Meteor.setTimeout);
+
+		const advertisement = peripheral.advertisement;
+		console.log('ad', advertisement);
+		Modules.insert({
+			_id: peripheral.id,
+			type: 'iPhone',
+			name: advertisement.localName
+		});
+
+		peripheral.connect(Meteor.bindEnvironment(function(error) {
+			peripheral.discoverServices(['cdd49cb83d1a11e6ac619e71128cae77'], Meteor.bindEnvironment(function(error, services) {
+				for (service of services) {
+					service.discoverCharacteristics(['b8d2aa983d1b11e6ac619e71128cae77'], Meteor.bindEnvironment(function(error, characteristics) {
+						for (characteristic of characteristics) {
+							characteristic.subscribe(function(error) {
+								console.log('error', error)
+							});
+							characteristic.on('data', Meteor.bindEnvironment(function(data) {
+								const event = {
+									senderId: peripheral.id,
+									type: 'gyro',
+									payload: [data.readInt32LE(0)/100000000, data.readInt32LE(4)/100000000, data.readInt32LE(8)/100000000],
+									date: new Date()
+								};
+								console.log(event);
+								Events.insert(event);
+							}))
+						}
+					}))
+				}
+			}))
+		}))
+	}
+}));
+
+function deleteTimeout(deviceID) {
+	clearTimeout(deviceTimeouts.get(deviceID));
+	deviceTimeouts.delete(deviceID);
+	Modules.remove(deviceID)
+}
+
+function removeAllModules() {
+	Modules.remove({type: 'bluetooth-device'})
+}
---
+++ Web application/packages/bluetooth-mediator/package.js
@@ -0,0 +1,29 @@
+Package.describe({
+  name: 'visualisation:bluetooth-mediator',
+  version: '0.0.1',
+  // Brief, one-line summary of the package.
+  summary: 'Adds discovered bluetooth devices',
+  // URL to the Git repository containing the source code for this package.
+  git: 'https://github.com/Quite-nice/Visualisation',
+  // By default, Meteor will default to using README.md for documentation.
+  // To avoid submitting documentation, set this field to null.
+  documentation: 'README.md'
+});
+
+Package.onUse(function(api) {
+  api.versionsFrom('1.3.3');
+  api.use(['ecmascript', 'mongo']);
+  api.use(['visualisation:database']);
+  api.mainModule('bluetooth-mediator.js', 'server');
+});
+
+Package.onTest(function(api) {
+  api.use('ecmascript');
+  api.use('tinytest');
+  api.use('visualisation:bluetooth-mediator');
+  api.mainModule('bluetooth-mediator-tests.js');
+});
+
+Npm.depends({
+  noble: "1.6.0"
+});
