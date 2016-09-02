dab0bee2faddb15424a06259a4403ba58e5ac358
Damiaan Dufaux <info@devian.tk>
2016-09-02 13:34:46 +0200

    update versions

--- Web application/packages/core/HISTORY.md
+++ Web application/packages/core/HISTORY.md
@@ -1,3 +1,7 @@
+# v0.0.8
+
+- update to `visualisation:core-ui@0.0.5`
+
 # v0.0.7
 
 - use `visualisation:generic-components@0.0.6`
--- Web application/packages/core/package.js
+++ Web application/packages/core/package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'visualisation:core',
-  version: '0.0.7',
+  version: '0.0.8',
   // Brief, one-line summary of the package.
   summary: 'Flexible visualisation system',
   // URL to the Git repository containing the source code for this package.
@@ -13,7 +13,7 @@ Package.describe({
 Package.onUse(function(api) {
   api.versionsFrom('1.3.3');
   api.use('ecmascript');
-  api.use(['visualisation:core-ui@0.0.5', 'visualisation:generic-components@0.0.6']);
+  api.use(['visualisation:core-ui@0.0.6', 'visualisation:generic-components@0.0.6']);
   api.mainModule('core.js');
 });
 
