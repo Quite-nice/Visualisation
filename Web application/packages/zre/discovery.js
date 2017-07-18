11b2529056a75eed058851721b084086a10e500e
Damiaan Dufaux <damiaan@dpro.be>
2017-07-18 14:14:23 +0200

    discover entering and leaving nodes

--- Web application/packages/zre/discovery.js
+++ Web application/packages/zre/discovery.js
@@ -1,10 +1,50 @@
-import Zyre from 'zyre.js'
+import { Meteor } from 'meteor/meteor'
 import { Events, Modules } from 'meteor/visualisation:database'
 import rootModule from './rootModule'
+import Zyre from 'zyre.js'
 
 const zreObserver = new Zyre({name: 'MeteorJS visualisation'})
 
+const zreNodeModuleType = 'ZRE node'
+const zreNodeModuleIdPrefix = 'ZRE-node:'
+
+Modules.remove({type: zreNodeModuleType})
+
+zreObserver.on('connect', Meteor.bindEnvironment((id, name, header) => {
+	console.log('zre node connected', id, name, header)
+
+	const visualisationID = visualisationIdFor(id)
+
+	Modules.upsert(visualisationID, {
+		$set: {
+			type: zreNodeModuleType,
+			header,
+			name,
+			parentId: rootModule._id
+		}
+	})
+
+	Events.insert({
+		senderId: visualisationID,
+		type: 'state',
+		payload: 2,
+		date: new Date()
+	})
+}))
+
+zreObserver.on('disconnect', Meteor.bindEnvironment((id, name) => {
+	const visualisationID = visualisationIdFor(id)
+
+	Events.insert({
+		senderId: visualisationID,
+		type: 'state',
+		payload: 0,
+		date: new Date()
+	})
+}))
+
 zreObserver.start(function() {
+	console.log('zre observer started')
 	Events.insert({
 		senderId: rootModule._id,
 		type: 'state',
@@ -12,3 +52,7 @@ zreObserver.start(function() {
 		date: new Date()
 	})
 })
+
+function visualisationIdFor(zreID) {
+	return zreNodeModuleIdPrefix + zreID
+}
