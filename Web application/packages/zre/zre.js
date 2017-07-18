7c935b93c50976cb28902e8f90584885826cffe9
Damiaan Dufaux <damiaan@dpro.be>
2017-07-18 12:05:31 +0200

    Add a zre module to the root

--- Web application/packages/zre/zre.js
+++ Web application/packages/zre/zre.js
@@ -1,5 +1,13 @@
 import Zyre from 'zyre.js'
+import {Events, Modules} from 'meteor/visualisation:database'
 
 // Variables exported by this module can be imported by other packages and
 // applications. See zre-tests.js for an example of importing.
-export const name = 'zre';
+export const name = 'zre'
+
+const zreObserver = new Zyre({name: 'MeteorJS visualisation'});
+
+Modules.upsert({type: 'ZRE network observer'}, {$set: {
+	parentId: null,
+	name: 'ZRE network'
+}});
